534ea590f0f801452677ce8f88445c88254a930a
Patricio Finschi <patofinschi@gmail.com>
2024-07-10 21:03:33 -0400

    Update urls.py

--- ProyWeb_Toro_Cabrera_Finschi_001D_EV3/ProyWeb_Toro_Cabrera_Finschi_001D_EV3/petanddogs/urls.py
+++ ProyWeb_Toro_Cabrera_Finschi_001D_EV3/ProyWeb_Toro_Cabrera_Finschi_001D_EV3/petanddogs/urls.py
@@ -27,6 +27,10 @@ urlpatterns = [
     path('logout/',views.exit, name='exit'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
     path('registrosAdd', views.registroAdd, name='registroAdd'),
+     path('agregar_al_carrito/<str:producto_id>', views.agregar_al_carrito, name='agregar_al_carrito'),
+    path('mostrar_carrito/', views.mostrar_carrito, name='mostrar_carrito'),
+    path('procesar_compra/', views.procesar_compra, name='procesar_compra'),
+    path('compras/', views.lista_compras, name='lista_compras'),
 
     path('accounts/password_reset/', PasswordResetView.as_view(template_name='registration/password_reset_form.html')),
     path('accounts/password_reset/done/', PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html')),
